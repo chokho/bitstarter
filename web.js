64e719abaf9a7dbb788a3f4738f9bf1603244ff2
Chokho Tan <cktan2g@gmail.com>
2013-07-13 14:10:48 +0200

    Modify to use index.html, try 9

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs  = require('fs');
 
-var dat = fs.readFileSync('index.html', 'utf8';
+var dat = fs.readFileSync('index.html', 'utf8');
 
 app.get('/', function(request, response) {
   response.send(dat);
