5fc4abce7dea4ca471e752f2f61be6a18e2eae93
Chokho Tan <cktan2g@gmail.com>
2013-07-13 12:45:57 +0200

    Modify to use index.html

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buff = new Buffer(20);
+
+fs.readFile('index.html'), function (err, buff) {
+  if (err) throw err;
+  console.log(data);
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
