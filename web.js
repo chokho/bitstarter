877b9cc9b0056378da72fdd6b4303b5f621a5ba8
Chokho Tan <cktan2g@gmail.com>
2013-07-13 13:27:00 +0200

    Modify to use index.html, try 6

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs = require('fs')
 
 var app = express.createServer(express.logger());
+var data = new Buffer(20);
 
 fs.readFile('index.html', 'utf8', function (err,data) {
   if (err) {
